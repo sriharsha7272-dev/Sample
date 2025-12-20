2f22bb5d6e9af6e34535d4e0e21ea0c8ce243096
Sri Harsha <psriharsha567@gmail.com>
2025-12-21 00:28:39 +0530

    Add React Native Android app and complete local setup

---
+++ mobile/metro.config.cjs
@@ -0,0 +1,5 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = mergeConfig(defaultConfig, {});
